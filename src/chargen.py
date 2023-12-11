1d6b2ac6e1d7ed4c3915c923b9875737da6f99b3
haaln <hassanan@kth.se>
2023-12-11 09:07:29 +0100

    gui

--- src/chargen.py
+++ src/chargen.py
@@ -23,22 +23,47 @@ if __name__ == '__main__':
 
 sg.set_options(text_justification='right')
 
+sg.theme('Dark')
 layout = [ [sg.Text('Char-gen v.0.01')],
+           [sg.Text('Main', font=('Helvetica', 15))],
            [sg.Checkbox('First name'), sg.Checkbox('Surname')],
+           [sg.Radio('Male', 1), sg.Radio('Female', 1)],
+#          [sg.Checkbox('Show advanced settings')],
+#          [sg.Checkbox('Length constraint'), sg.Spin(values=[i for i in range(1,16)], initial_value=9, size=(4,1))],
+#          [sg.Checkbox('Exclude characters'), sg.InputText(size=(10,1))],
+#          [sg.Checkbox('Include characters'), sg.InputText(size=(10,1))],
+#          [sg.Checkbox('Allow spaces'), sg.Checkbox('Replace spaces with underscores')],
+#          [sg.Checkbox('Begins with: '), sg.InputText(size=(10,1)), sg.Checkbox('Ends with: '), sg.InputText(size=(10,1))],
+#          [sg.Radio('Manually choose name', 4)],
+#          [sg.Checkbox('First name: '), sg.InputText(size=(10,1)), sg.Checkbox('Surname: '), sg.InputText(size=(10,1))],
+#          [sg.Radio('generate similar to: ', 4), sg.Text('First name: '), sg.InputText(size=(10,1)), sg.Text('Last name: '), sg.InputText(size=(10,1))],
            [sg.Text('_' * 100, size=(65,1))],
-           [sg.Text('Style')],
-           [sg.Radio('Simple algorithm')],
-           [sg.Radio('FFXIV')],
-           [sg.Radio('Realistic')],
-           [sg.Radio('Nickname')],
-           [sg.Radio('Dwarf'), sg.Radio('Elf')],
+           [sg.Text('Naming convention', font=('Helvetica', 15))],
+           [sg.Radio('Fantasy', 2), sg.Radio('Human', 6), sg.Radio('Dwarven', 6), sg.Radio('Elven', 6)],
+           [sg.Radio('FFXIV', 2), sg.Radio('Hyur', 3), sg.Radio('Lalafell', 3)],
+           [sg.Radio('Nickname', 2)],
+           [sg.Radio('Realistic', 2)],
+#          [sg.Radio('Historic', 2)],
+#          [sg.Radio('Nordic', 2)],
+#          [sg.Radio('Greek / Roman', 2)],
+#          [sg.Radio('Japanese', 2)],
+#          [sg.Radio('Korean', 2)],
+#          [sg.Radio('Chinese', 2)],
            [sg.Text('_' * 100, size=(65,1))],
-           [sg.CheckBox('Restrictions')],
-           [sg.Checkbox('Backstory')],
-           [sg.Submit(), sg.Cancel()] ]
+           [sg.Text('Additional features', font=('Helvetica', 15))],
+           # add conditional checkbox
+           [sg.Checkbox('Character restrictions'), sg.Checkbox('Objectives'), sg.Checkbox('Escape clause')],
+           [sg.Checkbox('Character backstory')],
+#          [sg.Checkbox('Character portrait'), sg.Text('include tags: '), sg.InputText(size=(10,1)), sg.Text('exclude tags: '), sg.InputText(size=(10,1))],
+           [sg.Text('_' * 100, size=(65,1))],
+           #[sg.Text('Generate', size=(8, 1)), sg.Spin(values=[i for i in range(1, 21)], initial_value=1, size=(4, 1)), sg.Text('character(s).')],
+           [sg.Button('Generate'), sg.Cancel()] ]
 
+#layout_tabs = [
+#                   [name('Tab, TabGroup'), sg.TabGroup([[sg.Tab('Tab1',[[sg.T(s=(15,2))]]), sg.Tab('Tab2', [[]])]])],]
+#    [sg.TabGroup([sg.Tab('Main'),[sg.T(s=(15,2))], sg.Tab('Character'), sg.Tab('Backstory'), sg.Tab('Misc') )]] ]
 
-window = sg.Window('character generator')
+window = sg.Window('character generator', layout)
 
 event, values = window.read()
 
